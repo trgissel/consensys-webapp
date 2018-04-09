32663bfb3a65b3d51a300d7e1a07225d18e5ebf0
Thomas Gissel <tom.gissel@tanium.com>
2018-04-08 21:01:41 -0400

    attempt fmt fix

--- server/controllers/carContractController.go
+++ server/controllers/carContractController.go
@@ -9,7 +9,7 @@ import (
 // EthereumClientConnect url should be in the form of "http://172.13.0.3:8545"
 func EthereumClientConnect(url string) {
 	// Create an IPC based RPC connection to a remote node and an authorized transactor
-	fmt.Println("attempting to connect to %s", url)
+	fmt.Println("attempting to connect to ", url)
 	_, err := ethclient.Dial(url)
 	if err != nil {
 		fmt.Println("Failed to connect to the Ethereum client: %v", err)
