8ea4b8e80037420585501207315bdfbf95ad7172
Thomas Gissel <tom.gissel@tanium.com>
2018-04-09 12:45:00 -0400

    change tx to txId

--- server/models/carContract.go
+++ server/models/carContract.go
@@ -3,5 +3,5 @@ package models
 // DeployContractTransaction represents a tran
 type DeployContractTransaction struct {
 	Address       string `json:"address"`
-	TransactionID string `json:"tx"`
+	TransactionID string `json:"txID"`
 }
