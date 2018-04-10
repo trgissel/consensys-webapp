40505520abd63e018ac8c3bdfd3b9cd4e1720438
Thomas Gissel <tom.gissel@tanium.com>
2018-04-10 16:21:41 -0400

    trying cancel again

--- server/controllers/transactionController.go
+++ server/controllers/transactionController.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"errors"
 	"log"
+	"time"
 
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/ethclient"
@@ -19,13 +20,17 @@ func GetTransactionDetails(url string, txHashString string) (TransactionDetails,
 	var txDetails TransactionDetails
 	txHash := common.HexToHash(txHashString)
 	client, err := ethclient.Dial(url)
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel() // releases resources if slowOperation completes before timeout elapses
+	var (
+		ctx    context.Context
+		cancel context.CancelFunc
+	)
+	ctx, cancel = context.WithTimeout(context.Background(), 1*time.Minute)
 	tx, _, err := client.TransactionByHash(ctx, txHash)
 	if err != nil {
 		log.Fatalf("Failed to find transaction by hash: %v", err)
 		return txDetails, errors.New("404")
 	}
 	txDetails.GasUsed = int(tx.Gas())
+	defer cancel()
 	return txDetails, nil
 }
