cd41dcf98835fc640961c70887937ab0c465da40
Thomas Gissel <tom.gissel@tanium.com>
2018-04-10 16:37:07 -0400

    try nested ctx

--- server/controllers/transactionController.go
+++ server/controllers/transactionController.go
@@ -15,6 +15,8 @@ type TransactionDetails struct {
 	GasUsed int `json:"gasUsed"`
 }
 
+type key int
+
 // GetTransactionDetails get transaction details
 func GetTransactionDetails(url string, txHashString string) (TransactionDetails, error) {
 	var txDetails TransactionDetails
@@ -23,8 +25,11 @@ func GetTransactionDetails(url string, txHashString string) (TransactionDetails,
 	d := time.Now().Add(1000 * time.Millisecond)
 	ctx, cancel := context.WithDeadline(context.Background(), d)
 	defer cancel()
-	tx, _, err := client.TransactionByHash(ctx, txHash)
+	const requestIDKey key = 0
+	ctx2 := context.WithValue(ctx, requestIDKey, txHash)
+	tx, _, err := client.TransactionByHash(ctx2, txHash)
 	if err != nil {
+		cancel()
 		log.Fatalf("Failed to find transaction by hash: %v", err)
 		return txDetails, errors.New("404")
 	}
